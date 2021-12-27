7f458d2d5ff8060ca9c545ae562dd908ad2ecf39
adibhossain <69015269+adibhossain@users.noreply.github.com>
2021-12-27 08:24:47 +0600

    swapped navbar, added back to top button

--- js/scripts.js
+++ js/scripts.js
@@ -139,6 +139,11 @@ var showX = function () {
     else document.getElementById('del-search').style.display = "block";
 };
 
+var showSearchBar = function () {
+    if(document.getElementById('discover-search-bar').style.display == "block") document.getElementById('discover-search-bar').style.display = "none";
+    else document.getElementById('discover-search-bar').style.display = "block";
+};
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -193,5 +198,31 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+    //Get the button
+    let mybutton = document.getElementById("btn-back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+    scrollFunction();
+    };
+
+    function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+
+    function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
 
 });
